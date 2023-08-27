5deb3ec02987e9d57a8dab673d2bbf157e0faf35
Suarez Josue <joosu16@gmail.com>
2023-08-27 17:39:21 -0300

    Added division to calculator

--- activity_1/calculator.py
+++ activity_1/calculator.py
@@ -1,6 +1,38 @@
 import addition as add
+
+
+def get_operand(name: str) -> float:
+    while True:
+        operand = input(f"Enter {name} operand: ").strip()
+        try:
+            return float(operand)
+        except ValueError:
+            print("Input is not a number. Please try again: ")
+
+
 def main():
-    print(add.addition(1, 2))
-    
+    while True:
+        operation = input("What operation you want to perform? (+ -):")
+        left_operand = get_operand("left")
+        right_operand = get_operand("right")
+
+        match operation:
+            case "+":
+                print(
+                    f"{left_operand} + {right_operand} = {add.addition(left_operand, right_operand)}")
+            case "/":
+                if right_operand == 0:
+                    print("Division by zero is not allowed.")
+                else:
+                    print(
+                    f"{left_operand} / {right_operand} = {left_operand / right_operand}")
+            case _:
+                print(
+                    "Operation {operation} is not supported. Please try again.", end="\n\n")
+
+        if input("Do you want to continue? (y/n): ").strip().lower() != "y":
+            break
+
+
 if __name__ == "__main__":
     main()
