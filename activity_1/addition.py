27f922fc89480ec623685f46b9404a0970f245ca
Fabo xw <fabianmartinezrincon.123@gmail.com>
2023-08-27 16:59:40 -0300

    update addition.py

--- activity_1/addition.py
+++ activity_1/addition.py
@@ -1,2 +1,18 @@
-def addition(y, x):
+def addition(x: int|float, y: int|float) -> int|float:
+    """Add two numbers.
+    
+    Args:
+        x (int): The first number to add.
+        y (int): The second number to add.
+        
+    Returns:
+        int: The sum of x and y.
+    """
     return x + y
+
+def main():
+    """Entry point for the program."""
+    print(addition(1, 2))
+    
+if __name__ == "__main__":
+    main()
