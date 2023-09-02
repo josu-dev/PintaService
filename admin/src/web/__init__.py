bf8d1250046ad6d49c57867b4aee42afbb9111f4
Lapeyre Iñaki <iniaki.lapeyre@gmail.com>
2023-09-02 15:32:27 -0300

    Añadi 404 Error y contact page

--- admin/src/web/__init__.py
+++ admin/src/web/__init__.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
 
 
-def create_app(env='development', static_folder='../../static'):  
+def create_app(env='development', static_folder='../../static'):
     app = Flask(__name__, static_folder=static_folder)
-    
+
     @app.get('/')
     def home():
         return render_template('home.html')
-      
-    return app
+    
+    @app.get('/contact')
+    def contact():
+        return render_template('contact.html')
+    
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('error.html', error_code='404', error_message='Página no encontrada'), 404
 
+    return app
