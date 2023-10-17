811e97c5776d51c817d535b7a7b76776bdae9154
Suarez Josue <joosu16@gmail.com>
2023-10-17 14:46:45 -0300

    Improved institution handling

--- admin/src/web/controllers/admin.py
+++ admin/src/web/controllers/admin.py
@@ -142,6 +142,126 @@ def toggle_active(user_id: int):
     return redirect("/admin/users")
 
 
+@bp.get("/institutions")
+@h.authenticated_route(module="institution", permissions=("index",))
+def show_institutions():
+    institutions = InstitutionService.get_institutions()
+
+    return render_template(
+        "admin/institutions/index.html", institutions=institutions
+    )
+
+
+@bp.get("/institutions/new")
+@h.authenticated_route(module="institution", permissions=("create",))
+def institutions_new_get():
+    form = InstitutionForm()
+
+    return render_template("admin/institutions/new.html", form=form)
+
+
+@bp.post("/institutions/new")
+@h.authenticated_route(module="institution", permissions=("create",))
+def institutions_new_post():
+    form = InstitutionForm(request.form)
+    if not form.validate():
+        return render_template("admin/institutions/new.html", form=form)
+
+    try:
+        institution = InstitutionService.create_institution(**form.values())
+    except InstitutionService.InstitutionServiceError as e:
+        # TODO: handle error on creation
+        h.flash_error(e.message)
+        return render_template("admin/institutions/new.html", form=form)
+
+    h.flash_success("Institución creada con éxito.")
+    return redirect(f"/admin/institutions/{institution.id}")
+
+
+@bp.get("/institutions/<int:institution_id>")
+@h.authenticated_route(module="institution", permissions=("show", "update"))
+def institutions_id_get(institution_id: int):
+    institution = InstitutionService.get_institution(institution_id)
+    if not institution:
+        h.flash_info(f"Institución con id {institution_id} no encontrada.")
+        return redirect("/admin/institutions")
+
+    form = InstitutionForm(obj=institution)
+    return render_template(
+        "admin/institutions/update.html",
+        form=form,
+        institution=institution,
+    )
+
+
+@bp.post("/institutions/<int:institution_id>")
+@h.authenticated_route(module="institution", permissions=("update",))
+def institutions_id_post(institution_id: int):
+    institution = InstitutionService.get_institution(institution_id)
+    if not institution:
+        h.flash_info(
+            f"Institución con id {institution_id} no encontrada al actualizar."
+        )
+        return redirect("/admin/institutions")
+
+    form = InstitutionForm(request.form)
+    if form.validate():
+        InstitutionService.update_institution(institution_id, **form.values())
+        h.flash_success("Institución actualizada con éxito.")
+        return redirect(f"/admin/institutions/{institution.id}")
+
+    return render_template(
+        "/admin/institutions/update.html", institution=institution, form=form
+    )
+
+
+@bp.post("/institutions/<int:institution_id>/delete")
+@h.authenticated_route(module="institution", permissions=("destroy",))
+def institution_delete_post(institution_id: int):
+    institution = InstitutionService.get_institution(institution_id)
+    if not institution:
+        h.flash_info(
+            f"Institución con id {institution_id} no encontrada al eliminar."
+        )
+        return redirect("/admin/institutions")
+
+    result = InstitutionService.delete_institution(institution_id)
+    if result:
+        h.flash_success("Institución eliminada con éxito.")
+        return redirect("/admin/institutions")
+
+    h.flash_error("No se pudo eliminar la institución.")
+    return redirect(f"/admin/institutions/{institution.id}")
+
+
+@bp.post("/institutions/<int:institution_id>/enable")
+@h.authenticated_route(module="institution", permissions=("activate",))
+def institution_id_activate_post(institution_id: int):
+    result = InstitutionService.update_institution(
+        institution_id, enabled=True
+    )
+    if result:
+        h.flash_success("Institución activada con éxito.")
+    else:
+        h.flash_error("No se pudo activar la institución.")
+
+    return redirect("/admin/institutions")
+
+
+@bp.post("/institutions/<int:institution_id>/disable")
+@h.authenticated_route(module="institution", permissions=("deactivate",))
+def institution_id_deactivate_post(institution_id: int):
+    result = InstitutionService.update_institution(
+        institution_id, enabled=False
+    )
+    if result:
+        h.flash_success("Institución desactivada con éxito.")
+    else:
+        h.flash_error("No se pudo desactivar la institución.")
+
+    return redirect("/admin/institutions")
+
+
 @bp.route("/create_service", methods=["GET", "POST"])
 def create_service():
     """Create a service form page"""
@@ -204,87 +324,5 @@ def service_edit_get(service_id: int):
         "service/setting.html",
         service=service,
         categories=categories,
-        form=form,
-    )
-
-
-@bp.route("/create_institution", methods=["GET", "POST"])
-def create_institution():
-    form = InstitutionForm(request.form)
-    if request.method == "POST":
-        InstitutionService.create_institution(**form.values())
-        return redirect("/admin/institutions")
-    return render_template("admin/create_institution.html", form=form)
-
-
-@bp.get("/institutions")
-@h.authenticated_route(module="institution", permissions=("index",))
-def show_institutions():
-    institutions = InstitutionService.get_institutions()
-    return render_template(
-        "admin/institutions.html", institutions=institutions
-    )
-
-
-@bp.get("/institutions/<int:institution_id>")
-@h.authenticated_route(module="institution", permissions=("show",))
-def institutions_dynamic_get(institution_id: int):
-    institution = InstitutionService.get_institution(institution_id)
-
-    if not institution:
-        flash("Institución no encontrada.", "error")
-        return redirect("/admin/institutions")
-
-    form = InstitutionForm(obj=institution)
-    return render_template(
-        "institutions/edit.html",
-        institution=institution,
-        form=form,
-    )
-
-
-@bp.post("/institution/<int:institution_id>/delete")
-def institution_delete_post(institution_id: int):
-    institution = InstitutionService.get_institution(institution_id)
-    if not institution:
-        flash("Institución no encontrada.", "error")
-        return redirect("/admin/institutions")
-    else:
-        InstitutionService.delete_institution(institution_id)
-        flash("Institución eliminada con éxito.", "success")
-    return redirect("/admin/institutions")
-
-
-@bp.post("/institution/<int:institution_id>/edit")
-def edit_institution_post(institution_id: int):
-    institution = InstitutionService.get_institution(institution_id)
-    if not institution:
-        flash("Institución no encontrada.", "error")
-        return redirect("/admin/institutions")
-
-    form = InstitutionForm(request.form)
-    if form.validate():
-        InstitutionService.update_institution(institution_id, **form.values())
-        flash("Institución actualizada con éxito.", "success")
-        return redirect("/admin/institutions")
-
-    return render_template(
-        "institutions/edit.html", institution=institution, form=form
-    )
-
-
-@bp.get("/institution/<int:institution_id>/edit")
-@h.authenticated_route()
-def institution_edit_get(institution_id: int):
-    institution = InstitutionService.get_institution(institution_id)
-
-    if not institution:
-        flash("Institución no encontrada.", "error")
-        return redirect("/admin/institutions")
-
-    form = InstitutionForm(obj=institution)
-    return render_template(
-        "institutions/edit.html",
-        institution=institution,
         form=form,
     )
