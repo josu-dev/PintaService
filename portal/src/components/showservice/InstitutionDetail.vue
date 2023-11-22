6daf7e26519c6a4df564bb4eb5315499182e63a7
Suarez Josue <joosu16@gmail.com>
2023-11-21 21:02:58 -0300

    Formated docs and updated old files

--- portal/src/components/showservice/InstitutionDetail.vue
+++ portal/src/components/showservice/InstitutionDetail.vue
@@ -1,50 +1,49 @@
-<template>
-    <div class="institution-detail">
-        <table>
-            <tr>
-                <td class="font-bold">Nombre:</td>
-                <td>&nbsp;&nbsp;{{ institution.name }}</td>
-            </tr>
-            <tr>
-                <td class="font-bold">Descripcion:</td>
-                <td>&nbsp;&nbsp;{{ institution.information }}</td>
-            </tr>
-            <tr>
-                <td class="font-bold">Direccion:</td>
-                <td>&nbsp;&nbsp;{{ institution.address }}</td>
-            </tr>
-            <tr>
-                <td class="font-bold">Palabras clave: </td>
-                <td>&nbsp;&nbsp;{{ institution.keywords }}</td>
-            </tr>
-            <tr>
-                <td class="font-bold">Web:</td>
-                <td>&nbsp;&nbsp;{{ institution.web }}</td>
-            </tr>
-            <tr>
-                <td class="font-bold">Email:</td>
-                <td>&nbsp;&nbsp;{{ institution.email }}</td>
-            </tr>
-            <tr>
-                <td class="font-bold">Horarios:</td>
-                <td>&nbsp;&nbsp;{{ institution.days_and_opening_hours }}</td>
-            </tr>
-            <tr>
-                <td class="font-bold">Habilitada:</td>
-                <td>&nbsp;&nbsp;{{ institution.enabled ? '✅' : '❌' }}</td>
-            </tr>
-        </table>
-    </div>
-</template>
-
 <script setup>
-const props = defineProps({
+  const props = defineProps({
     institution: {
-        type: Object,
-        default: () => { }
-    },
+      type: Object,
+      default: () => {}
+    }
+  });
 
-});
-
-defineEmits(['update:value']);
+  defineEmits(['update:value']);
 </script>
+
+<template>
+  <div class="institution-detail">
+    <table>
+      <tr>
+        <td class="font-bold">Nombre:</td>
+        <td>&nbsp;&nbsp;{{ institution.name }}</td>
+      </tr>
+      <tr>
+        <td class="font-bold">Descripcion:</td>
+        <td>&nbsp;&nbsp;{{ institution.information }}</td>
+      </tr>
+      <tr>
+        <td class="font-bold">Direccion:</td>
+        <td>&nbsp;&nbsp;{{ institution.address }}</td>
+      </tr>
+      <tr>
+        <td class="font-bold">Palabras clave:</td>
+        <td>&nbsp;&nbsp;{{ institution.keywords }}</td>
+      </tr>
+      <tr>
+        <td class="font-bold">Web:</td>
+        <td>&nbsp;&nbsp;{{ institution.web }}</td>
+      </tr>
+      <tr>
+        <td class="font-bold">Email:</td>
+        <td>&nbsp;&nbsp;{{ institution.email }}</td>
+      </tr>
+      <tr>
+        <td class="font-bold">Horarios:</td>
+        <td>&nbsp;&nbsp;{{ institution.days_and_opening_hours }}</td>
+      </tr>
+      <tr>
+        <td class="font-bold">Habilitada:</td>
+        <td>&nbsp;&nbsp;{{ institution.enabled ? '✅' : '❌' }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
--- portal/src/views/SearchServicesView.vue
+++ portal/src/views/SearchServicesView.vue
@@ -1,87 +1,96 @@
 <script setup>
-import { ref } from 'vue';
-import { APIService } from '@/utils/api';
-import { useRouter } from 'vue-router';
-/**
- * @typedef {{
- *    id: string,
- *    name: string,
- *    description: string,
- *    laboratory: string,
- *    keywords: string[],
- *    enabled: boolean,
- *    service_type: string,
- * }} ServiceData[]
- */
-/** @type {import('vue').Ref<ServiceData[]>} */
-const servicesList = ref([]);
-const qFilter = ref('');
-const typeFilter = ref('todos');
-const router = useRouter();
-const loading = ref(false);
+  import { APIService } from '@/utils/api';
+  import { ref } from 'vue';
+  import { useRouter } from 'vue-router';
+  /**
+   * @typedef {{
+   *    id: string,
+   *    name: string,
+   *    description: string,
+   *    laboratory: string,
+   *    keywords: string[],
+   *    enabled: boolean,
+   *    service_type: string,
+   * }} ServiceData[]
+   */
+  /** @type {import('vue').Ref<ServiceData[]>} */
+  const servicesList = ref([]);
+  const qFilter = ref('');
+  const typeFilter = ref('todos');
+  const router = useRouter();
+  const loading = ref(false);
 
-const filterServices = () => {
+  const filterServices = () => {
     loading.value = true;
     const url = `/services/search?q=${qFilter.value}&type=${typeFilter.value}`;
     APIService.get(url, {
-        onJSON(json) {
-            servicesList.value = json.data;
-            loading.value = false;
-        },
-        onFailure(response) {
-            console.log('Request failed');
-        },
-        onError(error) {
-            console.log(error);
-        }
+      onJSON(json) {
+        servicesList.value = json.data;
+        loading.value = false;
+      },
+      onFailure(response) {
+        console.log('Request failed');
+      },
+      onError(error) {
+        console.log(error);
+      }
     });
-};
-filterServices();
-const redirectToDetail = (serviceId) => {
+  };
+  filterServices();
+  const redirectToDetail = (serviceId) => {
     if (!serviceId) return;
     router.push(`/services/${serviceId}`);
-};
+  };
 </script>
 
 <template>
-    <div class="h-full overflow-y-auto">
-        <main class="text-center">
-            <h1 class="text-4xl font-bold text-primary mt-4 mb-4">Buscar Servicios</h1>
-            <div class="mx-auto max-w-md">
+  <div class="h-full overflow-y-auto">
+    <main class="text-center">
+      <h1 class="text-4xl font-bold text-primary mt-4 mb-4">Buscar Servicios</h1>
+      <div class="mx-auto max-w-md">
+        <div class="mb-4">
+          <label for="qFilter" class="block text-sm font-medium text-gray-700">Filtrar por:</label>
+          <input
+            type="text"
+            id="qFilter"
+            name="qFilter"
+            v-model="qFilter"
+            placeholder="Escribe aquí para filtrar"
+            class="mt-1 p-2 border rounded-md w-full"
+          />
+        </div>
 
-                <div class="mb-4">
-                    <label for="qFilter" class="block text-sm font-medium text-gray-700">Filtrar por:</label>
-                    <input type="text" id="qFilter" name="qFilter" v-model="qFilter" placeholder="Escribe aquí para filtrar"
-                        class="mt-1 p-2 border rounded-md w-full" />
-                </div>
+        <div class="mb-4">
+          <label for="typeFilter" class="block text-sm font-medium text-gray-700"
+            >Filtrar por tipo de servicio:</label
+          >
+          <select v-model="typeFilter" class="mt-1 p-2 border rounded-md w-full">
+            <option value="todos">Todos</option>
+            <option value="analisis">Análisis</option>
+            <option value="consultoria">Consultoría</option>
+            <option value="desarrollo">Desarrollo</option>
+          </select>
+        </div>
 
-                <div class="mb-4">
-                    <label for="typeFilter" class="block text-sm font-medium text-gray-700">Filtrar por tipo de
-                        servicio:</label>
-                    <select v-model="typeFilter" class="mt-1 p-2 border rounded-md w-full">
-                        <option value="todos">Todos</option>
-                        <option value="analisis">Análisis</option>
-                        <option value="consultoria">Consultoría</option>
-                        <option value="desarrollo">Desarrollo</option>
-                    </select>
-                </div>
+        <button @click="filterServices" class="btn btn-primary" :disabled="loading">
+          {{ loading ? 'Cargando...' : 'Filtrar' }}
+        </button>
+      </div>
 
-                <button @click="filterServices" class="btn btn-primary" :disabled="loading">
-                    {{ loading ? 'Cargando...' : 'Filtrar' }}
-                </button>
-            </div>
-
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-                <div v-for="service in servicesList" :key="service.id"
-                    class="bg-white rounded-lg overflow-hidden shadow-md">
-                    <div class="p-4 cursor-pointer" @click="redirectToDetail(service.id)">
-                        <h2 class="text-lg font-bold text-gray-800">Institución: {{ service.laboratory }}</h2>
-                        <h2 class="text-lg font-bold text-gray-800">Servicio: {{ service.name }}</h2>
-                        <p class="text-sm text-gray-600">Descripcion: {{ service.description }}</p>
-                        <p class="text-sm text-gray-600">Tipo: {{ service.service_type }}</p>
-                    </div>
-                </div>
-            </div>
-        </main>
-    </div>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+        <div
+          v-for="service in servicesList"
+          :key="service.id"
+          class="bg-white rounded-lg overflow-hidden shadow-md"
+        >
+          <div class="p-4 cursor-pointer" @click="redirectToDetail(service.id)">
+            <h2 class="text-lg font-bold text-gray-800">Institución: {{ service.laboratory }}</h2>
+            <h2 class="text-lg font-bold text-gray-800">Servicio: {{ service.name }}</h2>
+            <p class="text-sm text-gray-600">Descripcion: {{ service.description }}</p>
+            <p class="text-sm text-gray-600">Tipo: {{ service.service_type }}</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
