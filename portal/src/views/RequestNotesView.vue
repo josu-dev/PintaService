20c1ebfbdf59eda7e182d5cf596c001b1c1d70d6
Lapeyre Iñaki <iniaki.lapeyre@gmail.com>
2023-11-23 15:26:29 -0300

    Setup

---
+++ portal/src/views/RequestNotesView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="h-full overflow-y-auto">
+        <main class="grid grid-cols-1 sm:grid-cols-2 gap-8 px-2 mt-8">
+            <h1 class="text-4xl font-bold text-primary mt-4 mb-4">Notas del servicio</h1>
+
+        </main>
+    </div>
+</template>
